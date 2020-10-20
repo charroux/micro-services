81ec618890f831f4d91196ea6779dd6de90db1bc
orchaland <orchaland@gmail.com>
2020-10-20 11:47:56 +0200

    second service

--- microservice2/src/main/java/com/example/secondservice/SecondserviceApplication.java
+++ microservice2/src/main/java/com/example/secondservice/SecondserviceApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.secondservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
 @EnableCircuitBreaker
-public class Microservice2Application {
+public class SecondserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Microservice2Application.class, args);
+		SpringApplication.run(SecondserviceApplication.class, args);
 	}
 
 }
